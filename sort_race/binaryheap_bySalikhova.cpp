8b2c789c1342eed9ebf5d2fe372915275497adaf
Arius <37404674+Arius1@users.noreply.github.com>
2020-12-05 22:10:49 +0300

    reviewed by team lead

---
+++ sort_race/binaryheap_bySalikhova.cpp
@@ -0,0 +1,49 @@
+﻿
+#include <vector>
+#include <array>
+#include <iostream>
+using namespace std;
+
+
+void heap (vector<int>&data, int i, int n) //obrazovali heap
+{
+    int max = i;
+    
+    while (true)
+    {
+        int son = 2 * i + 1;//left son
+        if (son<n && data[son] > data[max])
+            max = son;
+        son++; // right son
+        if (son < n && data[son] > data[max])
+        max = son;
+        if (max == i)
+            break;
+        else
+        {
+            swap(data[max], data[i]);
+            i = max;
+        }
+
+    }
+}
+void heapsort(vector<int>& data)// sortirovka vectora
+{
+    int n = size(data);
+    for (int i = n / 2; i >= 0; i--)
+        heap(data, i , data.size());//cucha
+    for (int i = n - 1; i >= 1; i--)
+    {
+        swap(data[0], data[i]);
+        heap(data, 0, i);
+    }
+}
+
+vector<int> binaryheap(vector<int> data)
+{
+    heapsort(data);
+    return data;
+}
+
+
+
--- sort_race/sort_race.cpp
+++ sort_race/sort_race.cpp
@@ -3,6 +3,7 @@
 
 #include <array>
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -17,7 +18,7 @@ void Run(string method_name, Participant p, vector<int> data);
 
 //place your method name here
 vector<int> std_sort(vector<int>);
-
+vector<int> binaryheap(vector<int>);
 
 int main()
 {
@@ -26,6 +27,6 @@ int main()
     {
         auto data = GenerateData(n);
         RUN(std_sort);
-		//run your method here
+        RUN(binaryheap);
     }
 }
