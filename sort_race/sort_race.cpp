1af1e721b09048e5f4742a24fc7f0391e82db520
Arius <37404674+Arius1@users.noreply.github.com>
2020-12-07 17:23:03 +0300

    added menu for different data

--- sort_race/sort_race.cpp
+++ sort_race/sort_race.cpp
@@ -4,12 +4,15 @@
 #include <array>
 #include <vector>
 #include <string>
+#include <iostream>
+#include "utils.h"
 
 using namespace std;
 
 vector<int> GenerateData(int size, int max_value = INT_MAX);
 vector <int> readData(int size);
 vector <double> GenerateDoubleData(int size);
+
 using Participant = std::vector<int>(*)(std::vector<int>);
 using ParticipantDouble = std::vector<double>(*)(std::vector<double>);
 
@@ -29,15 +32,38 @@ vector<double> std_sort_double(vector<double>);
 
 vector <int> quickSort(vector <int>);
 vector <double> quickSortDouble(vector <double>);
+
 int main()
 {
     const array<int, 4> N = { 10, 1'000, 10'000, 1'000'000 };
-    for (int n : N)
-    {
-        //auto data = GenerateData(n);
-        auto data = GenerateDoubleData(n);
-        //auto data = readData(100);
-        RUNDouble(std_sort_double);
-        RUNDouble(quickSortDouble);
+        //выбрать необходимый генератор/чтение из файла
+    int i = getIntValue("Choose test type: \n 1)Int Generation \n 2)Double Generation \n 3)Read from file(100 elements): ", 1, 3);
+    switch (i) {
+        case 1: {
+            for (int n : N) {
+                auto data = GenerateData(n);
+                //place ur method here
+                RUN(std_sort);
+                RUN(quickSort);
+            }
+
+            break;
+        }
+        case 2: {
+            for (int n : N) {
+                auto data = GenerateDoubleData(n);
+                //place ur method here
+                RUNDouble(std_sort_double);
+                RUNDouble(quickSortDouble);
+            }
+            break;
+        }
+        case 3: {
+            auto data = readData(100);
+            //place ur method here
+            RUN(std_sort);
+            RUN(quickSort);
+
+        }
     }
 }
