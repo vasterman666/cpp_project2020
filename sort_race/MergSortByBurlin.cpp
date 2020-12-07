0d79cbe39ef1c65ce08dad59281456f42a28d79e
tetsu-kun <tetsu-kun@users.noreply.github.com>
2020-12-07 13:51:05 +0300

    CODE 2.0

--- sort_race/MergSortByBurlin.cpp
+++ sort_race/MergSortByBurlin.cpp
@@ -1,8 +1,56 @@
 #include <iostream>
+#include <iterator>
 #include <vector>
 
 using namespace std;
 
+void MergeSort(vector<int>& data, int first, int last)
+{
+	int left, right, middle, i; //в начале рассмотрим простейшие случаи
+	if (last - first < 2) // на случай если в векторе 1 или 0 эл.
+		return;
+	if (last - first == 2) //если в векторе 2 эл.
+	{
+		if (data[first] > data[first + 1])
+			swap(data[first], data[first + 1]);
+		return;
+	}
+
+	MergeSort(data, first, (first + last) / 2);
+	MergeSort(data, (first + last) / 2 + 1, last);
+
+	vector<int> vec;
+	middle = (first + last) / 2;
+	left = first;
+	right = middle + 1;
+
+	for (i = first; i <= last; i++)
+	{
+		if ((left <= middle) && ((right > last) || (data[left] < data[right])))
+		{
+			vec[i] = data[left];
+			left++;
+		}
+		else
+		{
+			vec[i] = data[right];
+			right++;
+		}
+	}
+	for (i = first; i <= last; i++)
+		data[i] = vec[i - first];
+}
+
+vector<int> merge_sort(vector<int> data)
+{
+	MergeSort(data, 0, data.size() - 1);
+	return data;
+}
+
+
+
+
+/*
 void merge(vector<int>& data, int first, int last) 
 {
 	int left_it, right_it, middle, j;
@@ -39,10 +87,12 @@ void MergeSort(vector<int>& data, int first, int last)
 			merge(data, first, last); //слияние двух частей
 		}
 	}
-};
+}; 
+
 
 vector<int> merge_sort(vector<int> data)
 {
 	MergeSort(data, 0,  data.size() - 1);
 	return data;
 }
+*/
--- sort_race/test_run.cpp
+++ sort_race/test_run.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <iostream>
 #include <algorithm>
+#include <fstream>
 
 using namespace std;
 
@@ -20,11 +21,35 @@ void Run(string method_name, Participant p, vector<int> data)
 		<< (is_sorted(res.begin(), res.end()) ? to_string(time) + "\tmcs" : "failed") << endl;
 }
 
+string getName()
+{
+	string name;
+	cout << "Write text name with .txt: ";
+	getline(cin, name);
+	return name;
+}
+
 vector<int> GenerateData(int size, int max_value = INT_MAX)
 {
 	//generate random data or read data from your file
 	vector<int> data(size);
 	for (int& d : data)
 		d = rand() % max_value;
+	return data;
+}
+
+vector<int> readData(int size) {
+	vector<int> data(size);
+	ifstream fin;
+	fin.open(getName(), ios::in);
+	if (fin.is_open()) {
+		for (int i = 0; i < size; i++) {
+			fin >> data.at(i);
+		}
+	}
+	else {
+		cout << "There is no such file! \n";
+	}
+	fin.close();
 	return data;
 }
