873da1b7e142d93e63475762084aa8faacc2961a
tetsu-kun <tetsu-kun@users.noreply.github.com>
2020-12-11 17:21:15 +0300

    READY CODE 1.1

--- sort_race/MergSortByBurlin.cpp
+++ sort_race/MergSortByBurlin.cpp
@@ -1,54 +1,56 @@
-#include <iostream>
+п»ї#include <iostream>
 #include <vector>
 
 using namespace std;
+//
+//template <typename T>
+//void MergeSort(vector<T>& data, size_t first, size_t last)
+//{
+//	// РІ РЅР°С‡Р°Р»Рµ СЂР°СЃСЃРјРѕС‚СЂРёРј РїСЂРѕSСЃС‚РµР№С€РёРµ СЃР»СѓС‡Р°Рё
+//	if (last - first < 2) //РµСЃР»Рё РІ РІРµРєС‚РѕСЂРµ 1 РёР»Рё 0 СЌР». (СѓР¶Рµ РѕС‚СЃРѕСЂС‚РёСЂРѕРІР°РЅ)
+//		return;
+//	if (last - first == 2) //РµСЃР»Рё РІ РІРµРєС‚РѕСЂРµ 2 СЌР».
+//	{
+//        T a = first + 1;
+//		if (data.at(first) > data.at(a))
+//			swap(data.at(first), data.at(a));
+//		return;
+//	}
+//
+//	MergeSort(data, first, first + (last - first) / 2);
+//	MergeSort(data, first + (last - first) / 2, last);
+//
+//	vector<int> v;
+//	T left = first;
+//	T middle = first + (last - first) / 2;
+//	T right = middle ;
+//
+//	while (v.size() < last - first)
+//	{
+//		if (left >= middle || (right < last && data.at(right) <= data.at(left)))
+//		{
+//			v.push_back(data.at(right));
+//			++right;
+//		}
+//		else
+//		{
+//			v.push_back(data.at(left));
+//			++left;
+//		}
+//	}
+//
+//	for (size_t i = first; i < last; ++i)
+//	{
+//	    T a = i - first;
+//		data.at(i) = v[a];
+//	}
+//}
 
-
-void MergeSort(vector<int>& data, size_t first, size_t last)
-{
-	//в начале рассмотрим проSстейшие случаи
-	if (last - first < 2) //если в векторе 1 или 0 эл. (уже отсортирован)
-		return;
-	if (last - first == 2) //если в векторе 2 эл.
-	{
-		if (data[first] > data[first + 1])
-			swap(data[first], data[first + 1]);
-		return;
-	}
-
-	MergeSort(data, first, first + (last - first) / 2);
-	MergeSort(data, first + (last - first) / 2, last);
-
-	vector<int> v;
-	size_t left = first;
-	size_t middle = first + (last - first) / 2;
-	size_t right = middle ;
-
-	while (v.size() < last - first)
-	{
-		if (left >= middle || (right < last && data[right] <= data[left]))
-		{
-			v.push_back(data.at(right));
-			++right;
-		}
-		else
-		{
-			v.push_back(data.at(left));
-			++left;
-		}
-	}
-
-	for (size_t i = first; i < last; ++i)
-	{
-		data[i] = v[i - first];
-	}
-}
-
-vector<int> merge_sort(vector<int> data)
-{
-	MergeSort(data, 0, data.size() - 1);
-	return data;
-}
+//vector<int> merge_sort(vector<int> data)
+//{
+//	MergeSort(data, 0, data.size() - 1);
+//	return data;
+//}
 
 /*
 vector<double> merge_sort_double(vector<double> data)
@@ -58,39 +60,40 @@ vector<double> merge_sort_double(vector<double> data)
 }
 */
 
-/*
-int main()
-{
-	vector<int> v;
-	for (int i = 0; i < 20; ++i)
-		v.push_back(i);
-	for (size_t i = 0; i < v.size(); ++i)
-		swap(v[i], v[rand() % (v.size() - i) + i]);
-	for (auto i : v)
-		cout << i << " ";
-	cout << endl;
 
-	MergeSort(v, 0, v.size());
-	for (auto i : v)
-		cout << i << " ";
-	cout << endl;
+//int main()
+//{
+//	vector<int> v;
+//	for (int i = 0; i < 20; ++i)
+//		v.push_back(i);
+//	for (size_t i = 0; i < v.size(); ++i)
+//		swap(v[i], v[rand() % (v.size() - i) + i]);
+//	for (auto i : v)
+//		cout << i << " ";
+//	cout << endl;
+//
+//	MergeSort(v, 0, v.size());
+//	for (auto i : v)
+//		cout << i << " ";
+//	cout << endl;
+//
+//}
 
-}
-*/
 
-/*
-void merge(vector<int>& data, int first, int last) 
+template <typename T>
+void merge(vector<T>& data, int first, int last) 
 {
 	int left_it, right_it, middle, j;
-	int *mas; //объявление указательного инта
-	mas = (int*)malloc((last - first) * sizeof(int)); //указатель инициализируется адресом первой интовой ячейкой из последовательности яечеек 
-	                                                 //(размер - разница последнего и первого элемента)
+	int *mas; //РѕР±СЉСЏРІР»РµРЅРёРµ СѓРєР°Р·Р°С‚РµР»СЊРЅРѕРіРѕ РёРЅС‚Р°
+	int a = last - first;
+	mas = (int*)malloc((a) * sizeof(int)); //СѓРєР°Р·Р°С‚РµР»СЊ РёРЅРёС†РёР°Р»РёР·РёСЂСѓРµС‚СЃСЏ Р°РґСЂРµСЃРѕРј РїРµСЂРІРѕР№ РёРЅС‚РѕРІРѕР№ СЏС‡РµР№РєРѕР№ РёР· РїРѕСЃР»РµРґРѕРІР°С‚РµР»СЊРЅРѕСЃС‚Рё СЏРµС‡РµРµРє 
+	                                                 //(СЂР°Р·РјРµСЂ - СЂР°Р·РЅРёС†Р° РїРѕСЃР»РµРґРЅРµРіРѕ Рё РїРµСЂРІРѕРіРѕ СЌР»РµРјРµРЅС‚Р°)
 
 	middle = (first + last) / 2;
 	left_it = first;
 	right_it = middle + 1;
 
-	for (j = first; j <= last; j++) //цикл, итератор которого пробегает от индекса начала до индекса конца
+	for (j = first; j <= last; j++) //С†РёРєР», РёС‚РµСЂР°С‚РѕСЂ РєРѕС‚РѕСЂРѕРіРѕ РїСЂРѕР±РµРіР°РµС‚ РѕС‚ РёРЅРґРµРєСЃР° РЅР°С‡Р°Р»Р° РґРѕ РёРЅРґРµРєСЃР° РєРѕРЅС†Р°
 		if ((left_it <= middle) && ((right_it > last) || (data[left_it] < data[right_it])))
 		{
 			*(mas + j - first) = data[left_it];
@@ -98,21 +101,22 @@ void merge(vector<int>& data, int first, int last)
 		}
 		else
 		{
-			*(mas + j - first) = data[left_it];
+			*(mas + j - first) = data[right_it];
 			right_it++;
 		}
-	//возвращение результата в список
+	//РІРѕР·РІСЂР°С‰РµРЅРёРµ СЂРµР·СѓР»СЊС‚Р°С‚Р° РІ СЃРїРёСЃРѕРє
 	for (j = first; j <= last; j++) data[j] = *(mas + j - first);
 };
-	
-void MergeSort(vector<int>& data, int first, int last)
+
+template <typename T>
+void MergeSort(vector<T>& data, int first, int last)
 {
 	{
 		if (first < last)
 		{
-			MergeSort(data, first, (first + last) / 2); //сортировка первой(левой) части
-			MergeSort(data, (first + last) / 2 + 1, last); //сортировка последней(правой) части
-			merge(data, first, last); //слияние двух частей
+			MergeSort(data, first, (first + last) / 2); //СЃРѕСЂС‚РёСЂРѕРІРєР° РїРµСЂРІРѕР№(Р»РµРІРѕР№) С‡Р°СЃС‚Рё
+			MergeSort(data, (first + last) / 2 + 1, last); //СЃРѕСЂС‚РёСЂРѕРІРєР° РїРѕСЃР»РµРґРЅРµР№(РїСЂР°РІРѕР№) С‡Р°СЃС‚Рё
+			merge(data, first, last); //СЃР»РёСЏРЅРёРµ РґРІСѓС… С‡Р°СЃС‚РµР№
 		}
 	}
 }; 
@@ -123,4 +127,3 @@ vector<int> merge_sort(vector<int> data)
 	MergeSort(data, 0,  data.size() - 1);
 	return data;
 }
-*/
