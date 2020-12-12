4d025463a2a5697e80a5ca429049ea24c5e19bb4
ktaishev <70592020+ktaishev@users.noreply.github.com>
2020-12-12 23:59:25 +0300

    Update sort_race.cpp

--- sort_race/sort_race.cpp
+++ sort_race/sort_race.cpp
@@ -1,4 +1,4 @@
-﻿// sort_race.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+// sort_race.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
 #include <array>
@@ -6,7 +6,7 @@
 #include <string>
 #include <iostream>
 #include "utils.h"
-
+#include "timsortByTaishev.cpp"
 
 using namespace std;
 
@@ -51,7 +51,7 @@ int main()
                 auto data = GenerateData(n);
                 //place ur method here
                 RUN(std_sort);
-                RUN(quickSort);
+                RUN(timsortByTaishev);
             }
 
             break;
@@ -61,7 +61,7 @@ int main()
                 auto data = GenerateDoubleData(n);
                 //place ur method here
                 RUNDouble(std_sort_double);
-                RUNDouble(quickSortDouble);
+                RUNDouble(timsortByTaishev);
             }
             break;
         }
@@ -69,8 +69,7 @@ int main()
             auto data = readData(100);
             //place ur method here
             RUN(std_sort);
-            RUN(quickSort);
-
+            RUN(timsortByTaishev);
         }
     }
 }
