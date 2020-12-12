eb98c32923cbcaef9be71865d2b402240eba3157
mshuranskiy <70566243+mshuranskiy@users.noreply.github.com>
2020-12-13 01:03:07 +0300

    Описание функции MergeSort

--- sort_race/MergeSort_Shuranskiy.cpp
+++ sort_race/MergeSort_Shuranskiy.cpp
@@ -5,14 +5,19 @@ using namespace std;
 
 
 template <typename T>
-void MergeMatrix(vector<T>& mass, size_t first, size_t last, size_t middle)
+void MergeMatrix(vector<T>& mass, size_t first, size_t last, size_t middle)//Функция слияния
 {
 
 }
 
 
 template <typename T>
-vector <T> MergeSort(vector<T> mass, size_t first, size_t last)
+vector <T> MergeSort(vector<T> mass, size_t first, size_t last)//Функция сортировки
 {
-	return mass;
+	if(last<=first)
+		return mass;
+	size_t middle = first + (last - first) / 2;
+	MergeSort(mass, first, middle);
+	MergeSort(mass, middle + 1, last);
+	MergeMatrix(mass, first, last, middle);
 }
