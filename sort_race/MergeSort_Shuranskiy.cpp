0fc737cb3c880f10a1c2fe0fab0f6da097d78a79
mshuranskiy <70566243+mshuranskiy@users.noreply.github.com>
2020-12-13 00:07:10 +0300

    1) Добавление своего файла для сортировки 2) Объявление функций требуемые для сортировок

---
+++ sort_race/MergeSort_Shuranskiy.cpp
@@ -0,0 +1,18 @@
+#include<vector>
+
+
+using namespace std;
+
+
+template <typename T>
+void MergeMatrix(vector<T>& mass, size_t first, size_t last, size_t middle)
+{
+
+}
+
+
+template <typename T>
+vector <T> MergeSort(vector<T> mass)
+{
+	return mass;
+}
