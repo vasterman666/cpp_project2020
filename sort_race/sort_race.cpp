eb3dad0d7a3a848654a8b0f90b017ca3b053ad20
mshuranskiy <70566243+mshuranskiy@users.noreply.github.com>
2020-12-13 19:46:23 +0300

    Добавление меню выбора входных данных для сортировки Добавление функций для сортировки вещественных массивов

--- sort_race/sort_race.cpp
+++ sort_race/sort_race.cpp
@@ -4,31 +4,88 @@
 #include <array>
 #include <vector>
 #include <string>
+#include <iostream>
 
 #include"MergeSort_Shuranskiy.h"
 
 using namespace std;
 
 vector<int> GenerateData(int size, int max_value = INT_MAX);
+vector<double> GenerateDataforDouble(int size, int max_value = INT_MAX);
 using Participant = std::vector<int>(*)(std::vector<int>);
+using Participantfordouble = std::vector<double>(*)(std::vector<double>);
 void Run(string method_name, Participant p, vector<int> data);
+void RunforDouble(string method_name, Participantfordouble p, vector<double> data);
 
 #define RUN(x) {                \
     Run(#x, x, data);           \
 }
 
+#define RUNforDouble(x) {       \
+    RunforDouble(#x, x, data);  \
+}
+
 //place your method name here
 vector<int> std_sort(vector<int>);
+vector<double> sort_for_integers_only(vector<double> data);
 
 
 int main()
 {
-    const array<int, 4> N = { 10, 1'000, 10'000, 1'000'000 };
-    for (int n : N)
+    setlocale(LC_ALL, "Russian");
+    cout << "1-Целочисленные массивы\n";
+    cout << "2-Вещественные массивы\n";
+    cout << "3-Наборы данных\n";
+    cout << "0-Выход\n";
+    cout << "Введите номер действия\n";
+    int i;
+    while(true)
     {
-        auto data = GenerateData(n);
-        RUN(std_sort);
-        RUN(MergeSort);
-		//run your method here
+        cin >> i;
+        if (cin.fail() || i < 0 || i>3)
+        {
+            cout << "Введённые данные не корректны\n";
+            cin.clear();
+            cin.ignore(32767, '\n');
+        }
+        else
+            break;
+
     }
+    switch (i)
+    {
+    case 1:
+    {
+        const array<int, 4> N = { 10, 1'000, 10'000, 1'000'000 };
+        for (int n : N)
+        {
+            auto data = GenerateData(n);
+            RUN(std_sort);
+            RUN(MergeSort);
+            //run your method here            
+        }
+        break;
+    }
+    case 2:
+    {
+        const array<int, 4> N = { 10, 1'000, 10'000, 1'000'000 };
+        for (int n : N)
+        {
+            auto data = GenerateDataforDouble(n);
+            RUNforDouble(sort_for_integers_only);
+            RUNforDouble(MergeSort);
+            //run your method here
+        }
+        break;
+    }
+    case 3:
+    {
+        break;
+    }
+    default:
+    {
+        break;
+    }
+    }
+    
 }
