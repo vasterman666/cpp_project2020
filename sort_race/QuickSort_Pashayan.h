7cda676cda989c26d1231a5ee851683dd0312e8b
arrtxxm <70713621+arrtxxm@users.noreply.github.com>
2020-12-15 23:03:34 +0300

    улучшение эффективности
    (в качестве опорного элемента медиана первого среднего и последнего элементов)

--- sort_race/QuickSort_Pashayan.h
+++ sort_race/QuickSort_Pashayan.h
@@ -4,10 +4,25 @@ using namespace std;
 
 template<typename T>
 int Separation(vector<T>& mass, int low, int high) { // –азбиение Ћомуто(low и high Ч соответственно, нижн€€ и верхн€€ границы сортируемого участка этого массива)
-    T x = mass[high]; // выбираетс€ последний элемент
+    //T pivot = mass[high]; // выбираетс€ последний элемент (использовалось в ранних реализаци€х алгоритма, снижало производительность на отсортированных массивах)
+    int mid;    
+    mid = (low + high) / 2;
+    if (mass[mid] < mass[low])
+    { 
+        swap (mass[low],mass[mid]);
+    }
+    if (mass[high] < mass[low])
+    {
+        swap(mass[low], mass[high]);
+    }
+    if (mass[mid] < mass[high])
+    {
+        swap(mass[high], mass[mid]);
+    }
+    T pivot = mass[high]; // дл€ улучшени€ эффективности в качестве опорного элемента выбираетс€ медиана первого, среднего и последнего элементов
     int less = low; // индекс less увеличиваетс€ каждый раз, когда находитс€ элемент, меньше или равный опорному => элемент вставл€етс€ перед опорным
     for (int i = low; i < high; ++i) {
-        if (mass[i] <= x) {
+        if (mass[i] <= pivot) {
             swap(mass[i], mass[less]);
             ++less;
         }
