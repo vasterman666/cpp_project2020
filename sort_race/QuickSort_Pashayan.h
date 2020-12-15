c83ffa99ec2080f947da0e0bba5d7b0f6c2dba74
arrtxxm <70713621+arrtxxm@users.noreply.github.com>
2020-12-15 22:10:39 +0300

    Update Quicksort
    Переименованы переменные, +комментарии

--- sort_race/QuickSort_Pashayan.h
+++ sort_race/QuickSort_Pashayan.h
@@ -3,26 +3,25 @@
 using namespace std;
 
 template<typename T>
-int Separation(vector<T>& mass, int l, int r) {
-    T x = mass[r];
-    int less = l;
-
-    for (int i = l; i < r; ++i) {
+int Separation(vector<T>& mass, int low, int high) { // Разбиение Ломуто(low и high — соответственно, нижняя и верхняя границы сортируемого участка этого массива)
+    T x = mass[high]; // выбирается последний элемент
+    int less = low; // индекс less увеличивается каждый раз, когда находится элемент, меньше или равный опорному => элемент вставляется перед опорным
+    for (int i = low; i < high; ++i) {
         if (mass[i] <= x) {
             swap(mass[i], mass[less]);
             ++less;
         }
     }
-    swap(mass[less], mass[r]);
-    return less;
+    swap(mass[less], mass[high]);
+    return less;    
 }
 
 template<typename T>
-void QuickSortImpl(vector<T>& mass, int l, int r) {
+void QuickSortImpl(vector<T>& mass, int l, int r) { 
     if (l < r) {
-        int q = Separation(mass, l, r);
-        QuickSortImpl(mass, l, q - 1);
-        QuickSortImpl(mass, q + 1, r);
+            int q = Separation(mass, l, r);
+            QuickSortImpl(mass, l, q - 1);
+            QuickSortImpl(mass, q + 1, r);
     }
 }
 
